f01d2a7f4655d0f7fba92a166a6835c5b704ce25
Redzikesxd <tomekreda16@gmail.com>
2018-04-09 11:24:46 +0200

    Panel Administartora

--- src/main/java/pl/orgella/repository/ProductRepository.java
+++ src/main/java/pl/orgella/repository/ProductRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.orgella.model.Product;
 
+import java.util.Set;
+
 @Repository
 public interface  ProductRepository extends JpaRepository<Product,Long> {
 
+    Set<Product> findAllByKategoria(String kategoria);
 }
